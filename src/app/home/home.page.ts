f465cc624cab0ba9c313030f198b9c4adec64cbe
salah3x <salah.loukili@gmail.com>
2019-09-05 18:13:12 +0100

    Add infinite scroll Closes #4

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { RepositoriesService } from './repositories.service';
+import { Repository } from './repository.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  repos: Repository[];
+  page = 1;
 
-  constructor() {}
+  constructor(private service: RepositoriesService) {}
 
+  ngOnInit() {
+    this.service.getRecentRepos().then(rs => (this.repos = rs));
+  }
+
+  loadRepos(event: any) {
+    this.page++;
+    this.service.getRecentRepos(this.page).then(rs => {
+      this.repos.push(...rs);
+      event.target.complete();
+    });
+  }
 }
